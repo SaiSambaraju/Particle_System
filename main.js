3129eaeffc4028687f1f3404cb7ea60a98c01d7f
Sai Sambaraju <saisam01@e126412.cambridge.arm.com>
2021-12-25 21:42:46 +0000

    Using various particle velocity for more realistic take off, fire also changes to smoke

--- main.js
+++ main.js
@@ -6,29 +6,30 @@ class ThrustParticleSystem{
         this.Emittingfrequency = frequency;
         this.thrustOf = belongingObject; //the object the thrust belongs to
         this.lifetime = lifetime;
-        this.gravity = 9;
+        this.gravity = 9;   //simple value no decimals yet
         this.geometry = new THREE.SphereGeometry(0.08, 10, 6);
-        this.material = new THREE.MeshBasicMaterial({color: 0xFF7700});
         this.thrustParticles = [];
         this.pos = initial_position;
         this.SetInitialParticlesPosAndVel(initial_position);
+        this.mass = 0.1;    //each particle is given a mass, in future iterations used to calculate force on rocket (rocket physics)
 
     }
 
 
     SetInitialParticlesPosAndVel(position){
-        //gives particles a (velocity vectorcurrently not used) and sets their initial position
-        //in future terations will be used for particles to follow a specific object, ie-> this.thrustof
-        var thrust_mesh = new THREE.Mesh(this.geometry, this.material);
-        thrust_mesh.position.x = position.x + (Math.random() * 1) + -0.5;            ;
-        thrust_mesh.position.y = position.y + (Math.random() * 1) + -0.5;
-        thrust_mesh.position.z = position.z + (Math.random() * 1) + -0.5;
+        //gives particles a velocity vector and sets their initial position
+        //initial position of particles is set using object it belongs to + some added randomness
+        var thrust_material = new THREE.MeshBasicMaterial({color: 0xFF7700});
+        var thrust_mesh = new THREE.Mesh(this.geometry, thrust_material);
+        thrust_mesh.position.x = position.x + (Math.random() * 0.3) + -0.15;            ;
+        thrust_mesh.position.y = position.y - 0.5 + (Math.random() * 0.3) + -0.1;
+        thrust_mesh.position.z = position.z + (Math.random() * 0.3) + -0.15;
         thrust_mesh.lifetime = this.lifetime;
-        thrust_mesh.velocityVector = new THREE.Vector3((Math.random() * 2) + -1,(Math.random() * 2) + -1,(Math.random() * 2) + -1);
-        thrust_mesh.lifelived = new THREE.Clock();
+        thrust_mesh.velocityVector = new THREE.Vector3((Math.random() * 0.04) + -0.02,(Math.random() * -0.1) + -0.05,(Math.random() * 0.04) + -0.02);
+        thrust_mesh.lifelived = new THREE.Clock();  //calculate lifetime of each particle
         this.thrustParticles.push(thrust_mesh);
         this.scene.add(thrust_mesh);
-        this.prevEmission = new THREE.Clock();
+        this.prevEmission = new THREE.Clock();      //separate clock to be used for generating new particles to meet frequency quota
     }
 
 
@@ -40,8 +41,32 @@ class ThrustParticleSystem{
         //write logic for travelling with rocket
         // for()//might need to update the list containing the particles before actually tranformng them? 
         for(let x = 0; x < this.thrustParticles.length; x++){
-            if(this.thrustParticles[x].lifetime > this.thrustParticles[x].lifelived.getElapsedTime()){
-                this.thrustParticles[x].translateY(-0.1);                
+            var particle_life_lived = this.thrustParticles[x].lifelived.getElapsedTime();
+            if(this.thrustParticles[x].lifetime > particle_life_lived){
+                if(particle_life_lived >= (this.lifetime/4)){//turn the firey particles into smoke after some of their lifetime
+                    this.thrustParticles[x].material.color.setHex(0x708c98);
+                    this.thrustParticles[x].smokeSpeedDamp = true;
+                }
+                if(this.thrustParticles[x].position.y <= 0.2){  //detecting collision with the ground, so particles bounce
+                    this.thrustParticles[x].needsBounce = true;  
+                }
+                if(this.thrustParticles[x].needsBounce){ //bounce from collision with the floor
+                    this.thrustParticles[x].translateX(this.thrustParticles[x].velocityVector.x);   
+                    this.thrustParticles[x].translateY((this.thrustParticles[x].velocityVector.y * -1) / this.gravity);  //dampening factor
+                    this.thrustParticles[x].translateZ(this.thrustParticles[x].velocityVector.z);   
+                }
+                else{
+                    if(this.thrustParticles[x].smokeSpeedDamp){ //smoke travels slower than the initial fire particle, so dampen speen
+                        this.thrustParticles[x].translateX((this.thrustParticles[x].velocityVector.x/2));     
+                        this.thrustParticles[x].translateY((this.thrustParticles[x].velocityVector.y)/2);     
+                        this.thrustParticles[x].translateZ((this.thrustParticles[x].velocityVector.z)/2);    
+                    }
+                    else{
+                        this.thrustParticles[x].translateX(this.thrustParticles[x].velocityVector.x);     
+                        this.thrustParticles[x].translateY(this.thrustParticles[x].velocityVector.y);     
+                        this.thrustParticles[x].translateZ(this.thrustParticles[x].velocityVector.z);     
+                    }        
+                }        
             }
             else{
                 this.scene.remove(this.thrustParticles[x]); //remove the particle from the scene 
@@ -50,14 +75,17 @@ class ThrustParticleSystem{
             }
             //while we are looping through we check the frequency and create new particles as appropriate.
             if((1/this.Emittingfrequency) <= this.prevEmission.getElapsedTime()){
-                this.createNew();
+                if (this.thrustOf != null){ //dont create any new thrust particles if rocket has gone
+                    this.createNew();
+                }
             }
         }
-        // console.log(this.thrustParticles.length); foe debbugging purposes
+    // console.log(this.thrustParticles.length); foe debbugging purposes
 
     }
 
 }
+
 
 
 
@@ -80,11 +108,15 @@ const grasstexture = new THREE.TextureLoader().load('textures/grassTexture.jpg')
 var material = new THREE.MeshBasicMaterial({map: grasstexture});    //a grass floor 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+const light = new THREE.AmbientLight(0xFFFFFF); //trying to add a moon \_(""/)_/
+scene.add(light);
 geometry = new THREE.CylinderGeometry(0, 0.5, 0.5, 7, 1);
-material = new THREE.MeshNormalMaterial();
+material = new THREE.MeshLambertMaterial();
 var rocket = new THREE.Mesh(geometry, material);    //a "rocket"
+rocket.mass = 10;
+rocket.fuel = 5;
 
-var thrustsys = new ThrustParticleSystem(scene, rocket, 1000, 1, new THREE.Vector3(0,0,0)); //particle system, thrust
+var thrustsys = new ThrustParticleSystem(scene, rocket, 1000, 1.5, new THREE.Vector3(0,0,0)); //particle system, thrust
 
 scene.add(rocket);
 
@@ -92,18 +124,32 @@ camera.position.set(5,5,5);
 controls.update();
 
 
-
+function compute_force_on_rocket(){
+    //how much fuel mass leaves rocket - depends the frequency of the thrust
+    //need a new measurement of time for equation to fully work
+    // 
+    //
+    //                  
+    //acceleration =  
+    //
+    //
+    //
+}
 
 
 function animate(){
     requestAnimationFrame(animate);
 
+    //compute_force_on_rocket();
+    //decrement fuel
+    //decrement rocket mass
     rocket.translateY(0.01);    //moving the rocket (simple for now)
     thrustsys.update(); //updating the particle system
 
     controls.update()   //updating orbital controls
 
-    if (rocket.position.y > 500){
+    if (rocket.position.y > 4){
+        thrustsys.thrustOf = null;
         scene.remove(rocket);   //removing rocket from scene after some distance
     }
 
